628b478b92e5f97e512b977002d767991b23fdc0
roninrp <rpglaznos@gmail.com>
2025-11-05 18:14:47 +0100

    added loss_analyses.py 1

--- codes/loss_analyses.py
+++ codes/loss_analyses.py
@@ -18,22 +18,28 @@ loss_df["loss_arr"] = [np.load(loss_path + x, allow_pickle=True) for x in loss_d
 # Find final loss values
 loss_df["loss_arr_fin"] = [x[-1] for x in loss_df.loss_arr]
 
-# # Create index for loss measured is scaled with y or not
-# loss_df["y_or_not"] = [0 if x[:18] == "base_test_en_val_l" else 1 for x in loss_df.loss_path]
-# # Find best config with lowest y scaled loss
-# best_y_config = loss_df[(loss_df.y_or_not == 1) & (loss_df[(loss_df.y_or_not == 1)].loss_arr_fin == loss_df[(loss_df.y_or_not == 1)].loss_arr_fin.min())].loss_path.iloc[0]
-# # Find best config for lowest loss
-# best_config = loss_df[(loss_df.y_or_not == 0) & (loss_df[(loss_df.y_or_not == 0)].loss_arr_fin == loss_df[(loss_df.y_or_not == 0)].loss_arr_fin.min())].loss_path.iloc[0]
 
-# # Define Corr dataframe
-# corr_df = pd.read_csv("../model_outputs/corrsM2e", sep="\t", names=["path", "corr_9", "mse_9", "corr_11", "mse_11", "corr_13", "mse_13", "corr_15", "mse_15", "best_epoch"])
-# # Find Corrs for lowest loss
-# print(corr_df[(corr_df.path == ("base_test_en_" + best_config[17:]))].to_string())
-# # Find Corrs for lowest y scaled loss
-# print(corr_df[(corr_df.path == ("base_test_en_" + best_y_config[19:]))].to_string())
 
 
 def get_config_corr(model_name: str):
+    """
+    Returns the best configuration and corresponding correlation and mse for the given model name.
+
+    Prameter:
+    ---------
+    
+    model_name = str name of the model
+
+    Returns:
+    --------
+    
+    Tuple: (best_config, corr, best_y_config, corr_y)
+    
+    best_config = Best config with lowest loss
+    corr = corresponding correlations and mse
+    best_y_config = Best config with lowest y-scaled loss
+    corr_y = corr = corresponding correlations and mse
+    """
     # Make dataframe for the given mode
     loss_model_df = loss_df[(loss_df.loss_path.apply(lambda x: x[:len(model_name) + 6]) == model_name + "_val_y") | (loss_df.loss_path.apply(lambda x: x[:len(model_name) + 6]) == model_name + "_val_l") ]. copy()
     # Create index for val loss measured is scaled with y or not
